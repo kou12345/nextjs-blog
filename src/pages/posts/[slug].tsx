dd060bd605ce722042367937c519b0d9f70f75db
kou12345 <hi.0412.ko.2002@gmail.com>
2023-06-13 14:26:53 +0900

    記事のタイトルとコンテンツを表示

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,5 +1,46 @@
-const Post: React.FC = () => {
-  return <div>コンテンツ</div>;
+import fs from "fs";
+import matter from "gray-matter";
+import { GetStaticProps } from "next";
+
+const folderPath = "public/posts";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const file = fs.readFileSync(
+    `${folderPath}/${context.params?.slug}.md`,
+    "utf-8"
+  );
+  const { data, content } = matter(file);
+  return { props: { frontMatter: data, content } };
+};
+
+export async function getStaticPaths() {
+  // index.tsxのgetStaticPropsとほぼ一緒。DRY原則
+  const files = fs.readdirSync(folderPath);
+  const paths = files.map((fileName) => ({
+    params: {
+      slug: fileName.replace(/\.md$/, ""),
+    },
+  }));
+  return {
+    paths,
+    fallback: false, // 存在しないページへのアクセスがあった場合に404 Not Foundを表示
+  };
+}
+
+type PostProps = {
+  frontMatter: {
+    title: string;
+  };
+  content: string;
+};
+
+const Post: React.FC<PostProps> = ({ frontMatter, content }) => {
+  return (
+    <div>
+      <h1>{frontMatter.title}</h1>
+      <div>{content}</div>
+    </div>
+  );
 };
 
 export default Post;
