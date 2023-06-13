257f894d5516c65e8deef6af435cc23a033a3983
kou12345 <hi.0412.ko.2002@gmail.com>
2023-06-13 12:34:43 +0900

    postsのfile pathを読み込み

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
+import fs from "fs";
+
+// ビルド時にデータを取得して事前にHTMLファイルのレンダリングを行うことでSSGとも呼ばれてる。
+export const getStaticProps = () => {
+  const posts = fs.readdirSync("public/posts");
+  console.log("files:", posts);
+  return {
+    props: {
+      posts: [],
+    },
+  };
+};
+
+const Home: React.FC = () => {
   return <div className="my-8">コンテンツ</div>;
-}
+};
+
+export default Home;
