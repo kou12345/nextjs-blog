fc57a717ff8fbf1811df59eb6a593a1ebdfa99e7
kou12345 <hi.0412.ko.2002@gmail.com>
2023-06-13 14:43:22 +0900

    イメージ画像を表示

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -2,6 +2,7 @@ import fs from "fs";
 import matter from "gray-matter";
 import { marked } from "marked";
 import { GetStaticProps } from "next";
+import Image from "next/image";
 
 const folderPath = "public/posts";
 
@@ -31,14 +32,23 @@ export async function getStaticPaths() {
 type PostProps = {
   frontMatter: {
     title: string;
+    image: string;
   };
   content: string;
 };
 
 const Post: React.FC<PostProps> = ({ frontMatter, content }) => {
   return (
-    <div className="prose">
-      <h1>{frontMatter.title}</h1>
+    <div className="prose prose-lg max-w-none">
+      <div className="border">
+        <Image
+          src={`/${frontMatter.image}`}
+          width={1200}
+          height={700}
+          alt={frontMatter.title}
+        />
+      </div>
+      <h1 className="mt-12">{frontMatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
     </div>
   );
